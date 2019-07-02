fd26cebd85d20e8e23ce1d63822d4315d10eab25
Alexandra Bakalova <Sasha_Bakalova@mail.ru>
2019-07-02 15:02:10 +0300

    camera follow added

---
+++ Blank_Spot/Assets/GoForThief.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoForThief : MonoBehaviour
+{
+    [SerializeField] GameObject followGameObject;
+
+    // Выполняется каждый кадр.
+    private void Update()
+    {
+        // Если игрок умер, ничего не делать.
+        if (!followGameObject)
+        {
+            return;
+        }
+
+        // Задаёт камере такую же позицию в плоскости (x, y), как у игрока.
+        transform.position = new Vector3(x: followGameObject.transform.position.x,
+                                         y: followGameObject.transform.position.y,
+                                         z: transform.position.z);
+    }
+
+}
