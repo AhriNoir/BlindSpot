cdef96593ea6ce5fef9b55dc80197691d624ec90
Esya-al <41239245+Esya-al@users.noreply.github.com>
2019-07-02 14:32:46 +0300

    Add files via upload

---
+++ Collision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+	public Movement movement;
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D collisionInfo) {
+		if (collisionInfo.gameObject.tag == "Player") {
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
---
+++ EndScreen.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour {
+
+
+	public void Death()
+	{
+		transform.position = new Vector3(x: 0, y: 0, z: transform.position.z);
+	
+	}
+}
